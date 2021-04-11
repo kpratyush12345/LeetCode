42d9010b565835aaad12d77f3ea0d1f5658b90cc
Pratyush Kumar <66811279+kpratyush12345@users.noreply.github.com>
2021-04-11 15:48:02 +0530

    Create 49_Group_Anagrams.cpp

---
+++ Strings/49_Group_Anagrams.cpp
@@ -0,0 +1,28 @@
+//49. Group Anagrams
+//https://leetcode.com/problems/group-anagrams/
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        string tmp;
+        vector<vector<string>> res;
+        unordered_map<string, int> um;
+        for (auto str: strs)
+        {
+            tmp = str;
+            sort(tmp.begin(), tmp.end());
+            if (um[tmp]>0)      
+            {
+                res[um[tmp]-1].push_back(str);
+            }
+            else
+            {
+                res.push_back({str});
+                um[tmp] = res.size();     
+            }
+            
+        }
+                
+        return res;
+    }
+};
