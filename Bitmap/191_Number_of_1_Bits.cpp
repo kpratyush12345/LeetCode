5995f3170c988595a51e6a8afcfb1a35c5fbb8ed
Pratyush Kumar <66811279+kpratyush12345@users.noreply.github.com>
2021-04-12 15:36:09 +0530

    Create 191_Number_of_1_Bits.cpp

---
+++ Bitmap/191_Number_of_1_Bits.cpp
@@ -0,0 +1,14 @@
+//191. Number of 1 Bits
+//https://leetcode.com/problems/number-of-1-bits/
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count=0;
+        while(n>0){
+            count+=(n&1);
+            n = n>>1;
+        }
+        return count;
+    }
+};
