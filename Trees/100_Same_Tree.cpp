1d8561bc94bdad496b2859438c3ea9527ef16f12
Pratyush Kumar <66811279+kpratyush12345@users.noreply.github.com>
2021-04-17 23:04:15 +0530

    Create 100_Same_Tree.cpp

---
+++ Trees/100_Same_Tree.cpp
@@ -0,0 +1,13 @@
+//100. Same Tree
+//https://leetcode.com/problems/same-tree/
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==NULL && q==NULL) return true;
+        if(p==NULL && q!=NULL) return false;
+        if(p!=NULL && q==NULL) return false;
+        if(p->val!=q->val) return false;
+        return isSameTree(p->right,q->right) && isSameTree(p->left,q->left);
+    }
+};
