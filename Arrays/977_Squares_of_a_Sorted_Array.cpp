1e6f496aea93ef5cf4949ee6f0286e21370fd739
Pratyush Kumar <66811279+kpratyush12345@users.noreply.github.com>
2021-05-14 12:47:59 +0530

    Create 977_Squares_of_a_Sorted_Array.cpp

---
+++ Arrays/977_Squares_of_a_Sorted_Array.cpp
@@ -0,0 +1,13 @@
+//977. Squares of a Sorted Array
+//https://leetcode.com/problems/squares-of-a-sorted-array/
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++){
+            nums[i] *= nums[i]; 
+        }
+        sort(nums.begin(),nums.end());
+        return nums;
+    }
+};
