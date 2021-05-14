7bf232a4e3e251b13422e292848321570f2ac130
Pratyush Kumar <66811279+kpratyush12345@users.noreply.github.com>
2021-05-14 12:45:28 +0530

    Create 485_Max_Consecutive_Ones.cpp

---
+++ Arrays/485_Max_Consecutive_Ones.cpp
@@ -0,0 +1,18 @@
+//485. Max Consecutive Ones
+//https://leetcode.com/problems/max-consecutive-ones/
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int cnt = 0;
+        int mx =0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1){
+                cnt++;
+                mx = max(mx,cnt);
+            }
+            else cnt=0;
+        }
+        return mx;
+    }
+};
